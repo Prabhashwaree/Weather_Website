73dab65c6f47c49ca04e4d51760d81e7d140a315
Prabhashearee <prabhashwaree57@gmail.com>
2022-10-11 04:12:41 +0530

    Update Request

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather-map27.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather-map27.p.rapidapi.com',
+
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'33fa680430mshf2e176b1ab906dcp1b153ajsn5da5a22004f1'
 };
 
 /*
